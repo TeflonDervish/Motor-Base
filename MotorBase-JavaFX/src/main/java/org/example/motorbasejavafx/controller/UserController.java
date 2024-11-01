9abbfefdb0dd03bb03ef9e862d7837c0109495b3
TeflonDervish <slava.semenov03@mail.ru>
2024-11-01 20:46:13 +0500

    make worker

--- MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/UserController.java
+++ MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/UserController.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 public class UserController {
 
 
+
+
     public void showObyavilation(ActionEvent actionEvent) {
         System.out.println("Сабина обосралась");
     }
