3de187dd7a1ec7a162dda162b040abcee96627fb
Sabina1715 <niganphantomhive@icloud.com>
2024-11-01 21:57:02 +0500

    Прописаны айдишники для элементов форм Юзер и Объявлейшн.

--- MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/UserController.java
+++ MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/UserController.java
@@ -8,4 +8,16 @@ public class UserController {
     public void showObyavilation(ActionEvent actionEvent) {
         System.out.println("Сабина обосралась");
     }
+
+    public void showPostObyavlation(ActionEvent actionEvent) {
+    }
+
+    public void search(ActionEvent actionEvent) {
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
+
+    public void save(ActionEvent actionEvent) {
+    }
 }
