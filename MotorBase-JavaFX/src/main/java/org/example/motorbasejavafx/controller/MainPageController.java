e89042cdfb2bee04c907bb69bddea2a2e7b0c5ee
Nail <nail.sharipov.2003@gmail.com>
2024-11-04 21:44:09 +0500

    главная страница

---
+++ MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/MainPageController.java
@@ -0,0 +1,8 @@
+package org.example.motorbasejavafx.controller;
+
+import javafx.event.ActionEvent;
+
+public class MainPageController {
+    public void onClickFind(ActionEvent actionEvent) {
+    }
+}
