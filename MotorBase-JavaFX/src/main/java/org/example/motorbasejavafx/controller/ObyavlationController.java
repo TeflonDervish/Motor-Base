8f6d9641fc996cc5d02b0b3efa52f9f8d19b6af7
Sabina1715 <niganphantomhive@icloud.com>
2024-11-06 20:33:39 +0500

    айдишники для объявлейшн и юзер прописаны

--- MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/ObyavlationController.java
+++ MotorBase-JavaFX/src/main/java/org/example/motorbasejavafx/controller/ObyavlationController.java
@@ -1,8 +1,35 @@
 package org.example.motorbasejavafx.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class ObyavlationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ObyavlationController implements Initializable {
+
+
+
+    @FXML
+    private TextField name;
+    @FXML
+    private ListView listParametr;
+    @FXML
+    private DatePicker datePublication;
+    @FXML
+    private ListView additionalDescription;
+    @FXML
+    private ListView dataSeller;
+    @FXML
+    private ImageView imageObyavlation;
+
+
     public void showPostObyvlation(ActionEvent actionEvent) {
     }
 
@@ -16,5 +43,10 @@ public class ObyavlationController {
     }
 
     public void datePublic(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        name.setText("Гибоз");
     }
 }
