19047604a7311183cb9b28efd3af6eb43895226f
TeflonDervish <slava.semenov03@mail.ru>
2024-11-27 21:32:33 +0500

    make

--- Motor-Base-Server/src/main/java/com/shesterki/Motor_Base_Server/repository/AnnouncementRepository.java
+++ Motor-Base-Server/src/main/java/com/shesterki/Motor_Base_Server/repository/AnnouncementRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
 
     List<Announcement> findByUserId(Long userId);
+    List<Announcement> findByDescriptionContaining(String description);
 }
